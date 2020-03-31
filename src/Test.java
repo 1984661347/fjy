0977870106ba0650a32c8a7d0e7e6191aa152c8b
Yan-del520 <yan0409@126.com>
2020-03-31 21:47:34 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+//		System.out.println("你是否爱学习");
+//		String str = in.next();
+//		if("是".equals(str)){
+//			System.out.println("咱们是好朋友");
+//			
+//		}else 
+//		if("勉强".equals(str)){
+//			System.out.println("算是朋友");
+//		}else
+//			System.out.println("不做朋友了");
+		System.out.println("你爱玩压缩吗？");
+		String str = in.next();
+		if("爱".equals(str)){
+			System.out.println("不带你玩");
+			System.out.println("带你玩");
+			
+		}
+	}
+}
